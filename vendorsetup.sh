2e7237ce1b81430a9c1ba6b4076c4e663b79cb1d
unreal3579 <unreal3579@bk.ru>
2023-06-22 15:46:10 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ziyi-user
-add_lunch_combo omni_ziyi-userdebug
-add_lunch_combo omni_ziyi-eng
+add_lunch_combo twrp_ziyi-user
+add_lunch_combo twrp_ziyi-userdebug
+add_lunch_combo twrp_ziyi-eng
