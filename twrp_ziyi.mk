fe25c535e49ccca4e51a6bd272764e817d4832c8
unreal3579 <unreal3579@bk.ru>
2023-06-23 23:59:20 +0300

    Update twrp_ziyi.mk

--- twrp_ziyi.mk
+++ twrp_ziyi.mk
@@ -4,21 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/xiaomi/ziyi
+
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+#$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ziyi device
-$(call inherit-product, device/xiaomi/ziyi/device.mk)
+$(call inherit-product, {DEVICE_PATH}/device.mk)
+
+PRODUCT_RELEASE_NAME := ziyi
 
 PRODUCT_DEVICE := ziyi
 PRODUCT_NAME := twrp_ziyi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 13 Lite
+PRODUCT_MODEL := Xiaomi 13 Lite
 PRODUCT_MANUFACTURER := xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+#PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
