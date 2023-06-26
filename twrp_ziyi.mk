4295e43d341473ce77c37498865161dba2c51e22
unreal3579 <unreal3579@bk.ru>
2023-06-26 17:34:33 +0300

    Update twrp_ziyi.mk

--- twrp_ziyi.mk
+++ twrp_ziyi.mk
@@ -4,14 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/ziyi
+#DEVICE_PATH := device/xiaomi/ziyi
 
 # Inherit from those products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
-#$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ziyi device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -24,13 +24,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2210129SG
 PRODUCT_MANUFACTURER := Xiaomi
 
-#PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := ziyi
+#TARGET_OTA_ASSERT_DEVICE := ziyi
 
 # Theme
-TW_STATUS_ICON_ALIGN := center
+#TW_STATUS_ICON_ALIGN := center
 #TW_Y_OFFSET :=
 #TW_H_OFFSET :=
 
