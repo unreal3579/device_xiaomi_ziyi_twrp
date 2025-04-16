52bd9ea296c83f1fdf593de154259d660a49b61d
unreal3579 <unreal3579@bk.ru>
2025-04-16 08:40:42 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_ziyi.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp-ziyi.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_ziyi-user \
-    twrp_ziyi-userdebug \
-    twrp_ziyi-eng
+    twrp-ziyi-user \
+    twrp-ziyi-userdebug \
+    twrp-ziyi-eng
